eef5e019f3185a8cac463545e3217c450e339895
MPikDev <het7ga@gmail.com>
2021-02-09 18:31:22 -0800

    added new model to save as a log for user that will add their changes to be able to revert if anything changes that we dont want

--- waittime/web_part/models.py
+++ waittime/web_part/models.py
@@ -4,7 +4,8 @@ from django.db import models
 
 # Create your models here.
 STATES_TUPLE = (('AK', 'Alaska'), ('AL', 'Alabama'), ('AR', 'Arkansas'), ('AS', 'American Samoa'), ('AZ', 'Arizona'), ('CA', 'California'), ('CO', 'Colorado'), ('CT', 'Connecticut'), ('DC', 'District of Columbia'), ('DE', 'Delaware'), ('FL', 'Florida'), ('GA', 'Georgia'), ('GU', 'Guam'), ('HI', 'Hawaii'), ('IA', 'Iowa'), ('ID', 'Idaho'), ('IL', 'Illinois'), ('IN', 'Indiana'), ('KS', 'Kansas'), ('KY', 'Kentucky'), ('LA', 'Louisiana'), ('MA', 'Massachusetts'), ('MD', 'Maryland'), ('ME', 'Maine'), ('MI', 'Michigan'), ('MN', 'Minnesota'), ('MO', 'Missouri'), ('MP', 'Northern Mariana Islands'), ('MS', 'Mississippi'), ('MT', 'Montana'), ('NA', 'National'), ('NC', 'North Carolina'), ('ND', 'North Dakota'), ('NE', 'Nebraska'), ('NH', 'New Hampshire'), ('NJ', 'New Jersey'), ('NM', 'New Mexico'), ('NV', 'Nevada'), ('NY', 'New York'), ('OH', 'Ohio'), ('OK', 'Oklahoma'), ('OR', 'Oregon'), ('PA', 'Pennsylvania'), ('PR', 'Puerto Rico'), ('RI', 'Rhode Island'), ('SC', 'South Carolina'), ('SD', 'South Dakota'), ('TN', 'Tennessee'), ('TX', 'Texas'), ('UT', 'Utah'), ('VA', 'Virginia'), ('VI', 'Virgin Islands'), ('VT', 'Vermont'), ('WA', 'Washington'), ('WI', 'Wisconsin'), ('WV', 'West Virginia'), ('WY', 'Wyoming'))
-
+SCHEDULING_OPTIONS = (('AP', 'Appointment'), ('FC', 'FCFS'))
+LOAD_TYPE_OPTIONS = (('PE', 'Perishable'), ('DR', 'Dry'), ('BO', 'Both'))
 # Appointment or FCFS dropdown
 
 class WebUserManager(BaseUserManager):
@@ -57,7 +58,6 @@ class CompanyInfo(models.Model):
     state = models.CharField(max_length=2, choices=STATES_TUPLE, default="WA")
     city = models.CharField(max_length=128)
     zip_code = models.IntegerField()
-    # product_category = list to choice from
     average_wait = models.FloatField()
     # max_wait = models.FloatField()
     # min_wait = models.FloatField()
@@ -67,73 +67,33 @@ class CompanyInfo(models.Model):
     overnight_parking = models.BooleanField(default=False)
     lumper = models.BooleanField(default=False)
     facilities = models.BooleanField(default=False)
-    scheduling = models.CharField(max_length=2, choices=STATES_TUPLE, default="WA")
-    load_type = models.CharField(max_length=2, choices=STATES_TUPLE, default="WA")
+    scheduling = models.CharField(max_length=2, choices=SCHEDULING_OPTIONS, default="AP")
+    load_type = models.CharField(max_length=2, choices=LOAD_TYPE_OPTIONS, default="PE")
 
 
     def __unicode__(self):
         return u"{0}, average_load{1}".format(self.company_name, self.average_load)
 
+class CompanyInfoEntry(models.Model):
 
+    # later a foreign key to the user that added it
+    company_name = models.CharField(max_length=48)
+    created_at = models.DateTimeField(auto_now_add=True)
+    open_time = models.DateTimeField()
+    close_time = models.DateTimeField()
+    address = models.CharField(max_length=128)
+    state = models.CharField(max_length=2, choices=STATES_TUPLE, default="WA")
+    city = models.CharField(max_length=128)
+    zip_code = models.IntegerField()
+    average_wait = models.FloatField()
+    # max_wait = models.FloatField()
+    # min_wait = models.FloatField()
+    average_load = models.FloatField()
+    # max_load = models.FloatField()
+    # min_load = models.FloatField()
+    overnight_parking = models.BooleanField(default=False)
+    lumper = models.BooleanField(default=False)
+    facilities = models.BooleanField(default=False)
+    scheduling = models.CharField(max_length=2, choices=SCHEDULING_OPTIONS, default="AP")
+    load_type = models.CharField(max_length=2, choices=LOAD_TYPE_OPTIONS, default="PE")
 
-
-
-states = {
-        'AK': 'Alaska',
-        'AL': 'Alabama',
-        'AR': 'Arkansas',
-        'AS': 'American Samoa',
-        'AZ': 'Arizona',
-        'CA': 'California',
-        'CO': 'Colorado',
-        'CT': 'Connecticut',
-        'DC': 'District of Columbia',
-        'DE': 'Delaware',
-        'FL': 'Florida',
-        'GA': 'Georgia',
-        'GU': 'Guam',
-        'HI': 'Hawaii',
-        'IA': 'Iowa',
-        'ID': 'Idaho',
-        'IL': 'Illinois',
-        'IN': 'Indiana',
-        'KS': 'Kansas',
-        'KY': 'Kentucky',
-        'LA': 'Louisiana',
-        'MA': 'Massachusetts',
-        'MD': 'Maryland',
-        'ME': 'Maine',
-        'MI': 'Michigan',
-        'MN': 'Minnesota',
-        'MO': 'Missouri',
-        'MP': 'Northern Mariana Islands',
-        'MS': 'Mississippi',
-        'MT': 'Montana',
-        'NA': 'National',
-        'NC': 'North Carolina',
-        'ND': 'North Dakota',
-        'NE': 'Nebraska',
-        'NH': 'New Hampshire',
-        'NJ': 'New Jersey',
-        'NM': 'New Mexico',
-        'NV': 'Nevada',
-        'NY': 'New York',
-        'OH': 'Ohio',
-        'OK': 'Oklahoma',
-        'OR': 'Oregon',
-        'PA': 'Pennsylvania',
-        'PR': 'Puerto Rico',
-        'RI': 'Rhode Island',
-        'SC': 'South Carolina',
-        'SD': 'South Dakota',
-        'TN': 'Tennessee',
-        'TX': 'Texas',
-        'UT': 'Utah',
-        'VA': 'Virginia',
-        'VI': 'Virgin Islands',
-        'VT': 'Vermont',
-        'WA': 'Washington',
-        'WI': 'Wisconsin',
-        'WV': 'West Virginia',
-        'WY': 'Wyoming'
-}
